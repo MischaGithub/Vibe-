6c0f24182c18b19343a51daed161a8fcb0a19c39
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-16 04:29:24 -1000

    changed the search and location bar to arrow functions

--- client/src/Components/layout/LocationBar.js
+++ client/src/Components/layout/LocationBar.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 
-class SearchBar extends Component {
-  render() {
-    return (
-      <form className="form">
-        <div className="search-wrapper">
-          <button className="btn btn-dark">Location</button>
-          <input type="text" name="text" placeholder="Where..." />
-        </div>
-      </form>
-    );
-  }
-}
+const SearchBar = () => {
+  return (
+    <form className="form">
+      <div className="search-wrapper">
+        <button className="btn btn-dark">Location</button>
+        <input type="text" name="text" placeholder="Where..." />
+      </div>
+    </form>
+  );
+};
 
 export default SearchBar;
--- client/src/Components/layout/SearchBar.js
+++ client/src/Components/layout/SearchBar.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-class SearchBar extends Component {
-  render() {
-    return (
-      <form className="form">
-        <div className="search-wrapper">
-          <button className="btn btn-dark">Search</button>
-          <input
-            type="text"
-            name="text"
-            placeholder="What are you looking for..."
-          />
-        </div>
-      </form>
-    );
-  }
-}
+const SearchBar = () => {
+  return (
+    <form className="form">
+      <div className="search-wrapper">
+        <button className="btn btn-dark">Search</button>
+        <input
+          type="text"
+          name="text"
+          placeholder="What are you looking for..."
+        />
+      </div>
+    </form>
+  );
+};
 
 export default SearchBar;
