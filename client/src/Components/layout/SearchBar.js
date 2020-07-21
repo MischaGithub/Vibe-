9e1c4b4ff63dc5d19f4161322d24071df7211353
MischaGithub <mischa.achmat120212@gmail.com>
2020-07-21 04:40:37 -1000

    converted searchbar to a functional component

--- client/src/Components/layout/SearchBar.js
+++ client/src/Components/layout/SearchBar.js
@@ -1,65 +1,67 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class SearchBar extends Component {
-  state = {
-    catergory: "",
-  };
-
-  // Proptypes
-  static propTypes = {
-    searchCategory: PropTypes.func.isRequired,
-    clearBusinesses: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired,
-  };
+const SearchBar = ({
+  searchCategory,
+  showClear,
+  clearBusinesses,
+  setAlert,
+}) => {
+  // State
+  const [catergory, setCatergory] = useState("");
 
   //  onChange
-  onChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
+  const onChange = (e) => {
+    setCatergory(e.target.value);
   };
 
   // onSubmit
-  onSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    if (this.state.catergory === "") {
-      this.props.setAlert("Please enter a category", "dark");
+    if (catergory === "") {
+      setAlert("Please enter a category", "dark");
     } else {
-      this.props.searchCategory(this.state.catergory);
-      this.setState({ catergory: "" });
+      searchCategory(catergory);
+      setCatergory("");
     }
   };
-  render() {
-    const { showClear, clearBusinesses } = this.props;
-    return (
-      <div className="conatiner">
-        <form onSubmit={this.onSubmit}>
-          <div className="search-wrapper">
-            <span className="search-span">Search</span>
-            <input
-              type="text"
-              name="catergory"
-              placeholder="What are you looking for..."
-              value={this.state.catergory}
-              onChange={this.onChange}
-            />
-            <button type="submit" className="search-btn">
-              <i className="fa fa-search" aria-hidden="true" />
-            </button>
-          </div>
-        </form>
-        {showClear && (
-          <button
-            className="clear-btn btn-dark btn-block"
-            onClick={clearBusinesses}
-          >
-            {" "}
-            Clear Search
+
+  return (
+    <div className="conatiner">
+      <form onSubmit={onSubmit}>
+        <div className="search-wrapper">
+          <span className="search-span">Search</span>
+          <input
+            type="text"
+            name="catergory"
+            placeholder="What are you looking for..."
+            value={catergory}
+            onChange={onChange}
+          />
+          <button type="submit" className="search-btn">
+            <i className="fa fa-search" aria-hidden="true" />
           </button>
-        )}
-      </div>
-    );
-  }
-}
+        </div>
+      </form>
+      {showClear && (
+        <button
+          className="clear-btn btn-dark btn-block"
+          onClick={clearBusinesses}
+        >
+          {" "}
+          Clear Search
+        </button>
+      )}
+    </div>
+  );
+};
+
+// Proptypes
+SearchBar.propTypes = {
+  searchCategory: PropTypes.func.isRequired,
+  clearBusinesses: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
